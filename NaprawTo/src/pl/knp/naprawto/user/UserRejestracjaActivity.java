6b724a466d5d7e696e8483a36c9a097d6a84f0ac
Norbert <Norbert@Norbert-Norbert>
2013-03-15 13:38:58 +0100

    Rejestracja formularz i aktywność początkowa

---
+++ NaprawTo/src/pl/knp/naprawto/user/UserRejestracjaActivity.java
@@ -0,0 +1,14 @@
+package pl.knp.naprawto.user;
+
+import pl.knp.naprawto.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserRejestracjaActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_rejestracja);
+	}
+}
