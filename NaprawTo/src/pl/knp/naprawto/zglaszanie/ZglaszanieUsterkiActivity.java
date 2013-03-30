4bab1d5a1deb102abba8eb0b70ce91a4a5adb62b
Norbert <Norbert@Norbert-Norbert>
2013-03-30 14:53:32 +0100

    Skonczone wyglad zglaszania usterki

--- NaprawTo/src/pl/knp/naprawto/zglaszanie/ZglaszanieUsterkiActivity.java
+++ NaprawTo/src/pl/knp/naprawto/zglaszanie/ZglaszanieUsterkiActivity.java
@@ -1,17 +1,50 @@
 package pl.knp.naprawto.zglaszanie;
 
 import pl.knp.naprawto.R;
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ScrollView;
 
-public class ZglaszanieUsterkiActivity extends Activity {
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class ZglaszanieUsterkiActivity extends MapActivity{
+	
+	MapView mapView;
+	ScrollView hsv;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);	
 		setContentView(R.layout.zglaszanie_usterki);
 		
+		mapView = (MapView)findViewById(R.id.mapview);
+		hsv = (ScrollView)findViewById(R.id.hsv);
 		
+		mapView.setOnTouchListener(new OnTouchListener() {
+		    @Override
+		    public boolean onTouch(View v, MotionEvent event) {
+		        switch (event.getAction()) {
+		            case MotionEvent.ACTION_MOVE:
+		                hsv.requestDisallowInterceptTouchEvent(true);
+		                break;
+		            case MotionEvent.ACTION_UP:
+		            case MotionEvent.ACTION_CANCEL:
+		                hsv.requestDisallowInterceptTouchEvent(false);
+		                break;
+		        }
+		        return mapView.onTouchEvent(event);
+		    }
+		});	
+	}
+
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
